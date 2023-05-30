cdae089c8f15e5b3c874fa89a711256e8a5738ae
Yannik Bauer <yannikbauer.1@gmail.com>
2023-05-30 12:49:09 +0100

    created file exercises/95b0d242-4a85-432f-b225-d718b0aa5da2/solutions/bad1e95f-9fe2-4bd8-b040-5c851c77989b/solution.py

---
+++ exercises/95b0d242-4a85-432f-b225-d718b0aa5da2/solutions/bad1e95f-9fe2-4bd8-b040-5c851c77989b/solution.py
@@ -0,0 +1,11 @@
+def print_flag(country):
+    if(country == 'Brazil'):
+        return '🟢💛🔵'
+    elif(country == 'USA'):
+        return '🔴⚪🔵'
+    elif(country == 'Italy'):
+        return '🟢⚪🔴'
+    elif(country == 'India'):
+        return '🟠⚪🟢'
+    else:
+        return 'Country flag not found.'
