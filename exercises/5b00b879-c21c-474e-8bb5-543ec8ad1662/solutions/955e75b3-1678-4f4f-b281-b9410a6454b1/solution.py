d527635aec2dd897bfbe5a814509902dfde5daa1
Yannik Bauer <yannikbauer.1@gmail.com>
2023-05-30 09:19:08 +0100

    Removed exercises/5b00b879-c21c-474e-8bb5-543ec8ad1662/solutions/955e75b3-1678-4f4f-b281-b9410a6454b1/solution.py

--- exercises/5b00b879-c21c-474e-8bb5-543ec8ad1662/solutions/955e75b3-1678-4f4f-b281-b9410a6454b1/solution.py
+++
@@ -1,6 +0,0 @@
-def is_pangram(sentence):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    for letter in alphabet:
-        if letter not in sentence.lower():
-            return False
-    return True
