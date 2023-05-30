8aa0d3341f65ca1d9c0da1271dc4a862ca66138f
Yannik Bauer <yannikbauer.1@gmail.com>
2023-05-30 15:01:24 +0100

    created file exercises/8ca8aab8-92b8-4236-89d1-ddc82412b4ad/solutions/d40ed513-dd40-4ed8-9315-ab93b432fd49/solution.py

---
+++ exercises/8ca8aab8-92b8-4236-89d1-ddc82412b4ad/solutions/d40ed513-dd40-4ed8-9315-ab93b432fd49/solution.py
@@ -0,0 +1,12 @@
+def prime_factors(n):
+    result = []
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            result.append(i)
+    if n > 1:
+        result.append(n)
+    return result
