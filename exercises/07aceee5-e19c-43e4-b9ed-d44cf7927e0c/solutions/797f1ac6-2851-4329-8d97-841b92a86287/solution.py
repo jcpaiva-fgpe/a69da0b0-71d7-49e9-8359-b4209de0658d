05fc03e1ea1f05980320f645635fd04a1eb7e8a4
Yannik Bauer <yannikbauer.1@gmail.com>
2023-05-30 15:00:43 +0100

    created file exercises/07aceee5-e19c-43e4-b9ed-d44cf7927e0c/solutions/797f1ac6-2851-4329-8d97-841b92a86287/solution.py

---
+++ exercises/07aceee5-e19c-43e4-b9ed-d44cf7927e0c/solutions/797f1ac6-2851-4329-8d97-841b92a86287/solution.py
@@ -0,0 +1,8 @@
+def validate_ip_address(ip_address):
+    parts = ip_address.split('.')
+    if len(parts) != 4:
+        return False
+    for part in parts:
+        if not part.isdigit() or int(part) < 0 or int(part) > 255:
+            return False
+    return True
