1d3621a453b076506a992dbb1221ed556963c4d0
Yannik Bauer <yannikbauer.1@gmail.com>
2023-05-30 09:19:07 +0100

    Removed exercises/0716af18-30f4-448b-a468-2870842aec77/solutions/6b754d20-1931-4db2-af13-3438f5301939/solution.py

--- exercises/0716af18-30f4-448b-a468-2870842aec77/solutions/6b754d20-1931-4db2-af13-3438f5301939/solution.py
+++
@@ -1,7 +0,0 @@
-def number_converter(number, base):
-    if base == 2:
-        return bin(number)[2:]
-    elif base == 8:
-        return oct(number)[2:]
-    elif base == 16:
-        return hex(number)[2:].upper()
