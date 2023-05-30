26c248da1b34e03f1bf13276eccec996c4a4a356
Yannik Bauer <yannikbauer.1@gmail.com>
2023-05-30 16:18:51 +0100

    created file exercises/b4eea6af-bce4-4f4b-af5b-0584a0c483bf/solutions/764299f2-6c6f-41b4-9997-4de4a473c9e5/solution.py

---
+++ exercises/b4eea6af-bce4-4f4b-af5b-0584a0c483bf/solutions/764299f2-6c6f-41b4-9997-4de4a473c9e5/solution.py
@@ -0,0 +1,11 @@
+def simple_calculator(num1, operator, num2):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1 / num2
+    else:
+        return "Invalid operator"
