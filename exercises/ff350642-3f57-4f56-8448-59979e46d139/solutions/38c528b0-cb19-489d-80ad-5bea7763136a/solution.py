f7a11c595e05dcf9aff871239c62b42efd1b60be
Yannik Bauer <yannikbauer.1@gmail.com>
2023-05-30 13:25:01 +0100

    created file exercises/ff350642-3f57-4f56-8448-59979e46d139/solutions/38c528b0-cb19-489d-80ad-5bea7763136a/solution.py

---
+++ exercises/ff350642-3f57-4f56-8448-59979e46d139/solutions/38c528b0-cb19-489d-80ad-5bea7763136a/solution.py
@@ -0,0 +1,12 @@
+def fizzbuzz(n):
+    result = ''
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            result += 'FizzBuzz '
+        elif i % 3 == 0:
+            result += 'Fizz '
+        elif i % 5 == 0:
+            result += 'Buzz '
+        else:
+            result += str(i) + ' '
+    return result.strip()
