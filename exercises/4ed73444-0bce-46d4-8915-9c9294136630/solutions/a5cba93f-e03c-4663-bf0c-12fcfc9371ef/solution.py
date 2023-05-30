2b702889b2ae8a085ab8ee993d9ff41c78f6522d
Yannik Bauer <yannikbauer.1@gmail.com>
2023-05-30 16:19:23 +0100

    Removed exercises/4ed73444-0bce-46d4-8915-9c9294136630/solutions/a5cba93f-e03c-4663-bf0c-12fcfc9371ef/solution.py

--- exercises/4ed73444-0bce-46d4-8915-9c9294136630/solutions/a5cba93f-e03c-4663-bf0c-12fcfc9371ef/solution.py
+++
@@ -1,5 +0,0 @@
-def temperature_converter(unit, value):
-    if unit.lower() == 'celsius':
-        return round((value * 9/5) + 32, 2)
-    elif unit.lower() == 'fahrenheit':
-        return round((value - 32) * 5/9, 2)
