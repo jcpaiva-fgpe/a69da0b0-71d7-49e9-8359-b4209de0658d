d1f471a806c20e7bb0e1fcdac4e60aa5ca3c38fc
Yannik Bauer <yannikbauer.1@gmail.com>
2023-05-30 15:00:05 +0100

    created file exercises/70df6a99-012f-404c-9d7b-273f5218094f/solutions/3d180b00-fc1b-403e-be4a-114af8772a38/solution.py

---
+++ exercises/70df6a99-012f-404c-9d7b-273f5218094f/solutions/3d180b00-fc1b-403e-be4a-114af8772a38/solution.py
@@ -0,0 +1,8 @@
+def multiplication_table(n):
+    table = []
+    for i in range(1, n+1):
+        row = []
+        for j in range(1, n+1):
+            row.append(i*j)
+        table.append(row)
+    return table
