c875eff3778a385a088856ac8cb94eeb581029cb
Yannik Bauer <yannikbauer.1@gmail.com>
2023-05-30 10:13:07 +0100

    created file exercises/b2bc4743-aec5-4180-ab3d-fc4e8196d90d/solutions/dddfcf3c-3437-424a-ba54-9bb5fa2e95e8/solution.py

---
+++ exercises/b2bc4743-aec5-4180-ab3d-fc4e8196d90d/solutions/dddfcf3c-3437-424a-ba54-9bb5fa2e95e8/solution.py
@@ -0,0 +1,24 @@
+def minimum_jumps(arr):
+    if len(arr) == 1:
+        return 0
+
+    jumps = 1
+    max_reach = arr[0]
+    steps = arr[0]
+
+    for i in range(1, len(arr)):
+        if i == len(arr) - 1:
+            return jumps
+
+        max_reach = max(max_reach, i + arr[i])
+        steps -= 1
+
+        if steps == 0:
+            jumps += 1
+
+            if i >= max_reach:
+                return -1
+
+            steps = max_reach - i
+
+    return -1
